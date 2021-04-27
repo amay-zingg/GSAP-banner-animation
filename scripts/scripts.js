37130ed22e22c2565ce7b91bfab342ed9fcab102
amay-zingg <anastasiamay29@gmail.com>
2021-04-26 23:58:57 -0400

    frame 1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,20 @@
-TweenMax.to($('.circle'), 2, {x: 150, y: 150, borderRadius: '40%' });
 
-// * * * * UNDERSTANDING LABELS
-// Used labels to make animations execute at the same time
-var tlanimation3 = new TimelineMax();
+// .banner-ad
+// .frame-1
 
-tlanimation3.to('.circle2', 1, {x: 100, ease: Elastic.easeOut}, 'circleRectangle')
-.to('.square2', 1, {x: 100, ease: Back.easeIn})
-.to('.rectangle2', 1, {x: 100, ease: Elastic.easeOut}, 'circleRectangle');
+// .logo-face
+
+/* * * * * * * * * * * * * * FRAME 1 * * * * * * * * * * * * * * */
+/* * * * Red BG - fade in lion face * * * */
+TweenMax.to('.logo-face', 3, { scale: 1.2, opacity: 1, ease: Back.easeIn});
+
+
+// TweenMax.to($('.circle'), 2, {x: 150, y: 150, borderRadius: '40%' });
+
+// // * * * * UNDERSTANDING LABELS
+// // Used labels to make animations execute at the same time
+// var tlanimation3 = new TimelineMax();
+
+// tlanimation3.to('.circle2', 1, {x: 100, ease: Elastic.easeOut}, 'circleRectangle')
+// .to('.square2', 1, {x: 100, ease: Back.easeIn})
+// .to('.rectangle2', 1, {x: 100, ease: Elastic.easeOut}, 'circleRectangle');
