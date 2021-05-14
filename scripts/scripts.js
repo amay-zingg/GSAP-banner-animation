aa6138c4094555fa1838b37d59acd6c7987244aa
amay-zingg <anastasiamay29@gmail.com>
2021-05-13 23:42:37 -0400

    Made a bunch of changes and made nav of old versions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,150 @@
+var creative = {}; //ad object
 
-// .banner-ad
-// .frame-1
+function init() {
+  console.log("Ad Ready");
+  creative.ad = document.getElementById("viewport");
 
-// .logo-face
+  gsap.set(["#viewport", "#border"], { autoAlpha: 1 });
+  gsap.set(["#template"], { alpha: 0.4 });
 
-/* * * * * * * * * * * * * * FRAME 1 * * * * * * * * * * * * * * */
-/* * * * Red BG - fade in lion face * * * */
-TweenMax.to('.logo-face', 3, { scale: 1.2, opacity: 1, ease: Back.easeIn});
+  frameOne();
+}
 
+function frameOne() {
+  /* Set frame for action */
+  gsap.set(["#GL_bg1-1", "#F1_image1-1"], { opacity: 1 });
 
-// TweenMax.to($('.circle'), 2, {x: 150, y: 150, borderRadius: '40%' });
+  /* Frame animation */
+  gsap.from(["#F1_image1-1"], {
+    duration: 0.4,
+    scale: 10,
+    opacity: 0,
+    ease: "back.out(0.7)",
+  });
 
-// // * * * * UNDERSTANDING LABELS
-// // Used labels to make animations execute at the same time
-// var tlanimation3 = new TimelineMax();
+  var frameDelay = 1.5;
+  /* Set frame exit transition */
+  gsap.set(["#F1_image1-1"], { opacity: 0, delay: frameDelay });
+  gsap.delayedCall(frameDelay, frameTwo);
+}
 
-// tlanimation3.to('.circle2', 1, {x: 100, ease: Elastic.easeOut}, 'circleRectangle')
-// .to('.square2', 1, {x: 100, ease: Back.easeIn})
-// .to('.rectangle2', 1, {x: 100, ease: Elastic.easeOut}, 'circleRectangle');
+function frameTwo() {
+  /* Set frame for action */
+  gsap.set(["#F2_image1-1"], { alpha: 1 });
+  gsap.set(["#F2_image2-1"], { alpha: 1, rotation: 5 });
+
+  /* Frame animation */
+  gsap.from(["#F2_image1-1"], {
+    duration: 5,
+    scale: 1.1,
+    force3D: false,
+    rotation: 0.01,
+  });
+
+  var frameDelay = 2.5;
+  /* Set frame exit transition */
+  gsap.set(["#F2_image1-1", "#F2_image2-1"], { alpha: 0, delay: frameDelay });
+  gsap.delayedCall(frameDelay, frameThree);
+}
+
+function frameThree() {
+  /* Set frame for action */
+  gsap.set(["#F3_text1-1"], { alpha: 1 });
+
+  /* Frame animation */
+  gsap.from(["#F3_text1-1"], { duration: 0.5, alpha: 0, x: -10 });
+
+  var frameDelay = 1.5;
+  /* Set frame exit transition */
+  gsap.set(["#F3_text1-1"], { alpha: 0, delay: frameDelay });
+  gsap.delayedCall(frameDelay, frameFour);
+}
+
+function frameFour() {
+  /* Set frame for action */
+  gsap.set(["#F4_text1-1", "#F4_text1-2", "#F4_image1-1"], { alpha: 1 });
+  gsap.set(["#F4_image2-1"], { alpha: 1, rotation: -8 });
+
+  /* Frame animation */
+  gsap.to(["#F4_image1-1"], {
+    duration: 5,
+    scale: 1.1,
+    force3D: false,
+    rotation: 0.01,
+  });
+
+  var tl = gsap.timeline({});
+  tl.from(["#F4_image2-1"], { duration: 0.5, y: 250 });
+  tl.from(["#F4_text1-1"], { duration: 0.5, x: -3, alpha: 0 }, "-=0.3");
+  tl.from(["#F4_text1-2"], { duration: 0.5, x: -3, alpha: 0 }, "-=0.3");
+
+  var frameDelay = 3.5;
+  /* Set frame exit transition */
+  gsap.set(["#F4_text1-1", "#F4_text1-2", "#F4_image1-1", "#F4_image2-1"], {
+    alpha: 0,
+    delay: frameDelay,
+  });
+  gsap.delayedCall(frameDelay, frameFive);
+}
+
+function frameFive() {
+  /* Set frame for action */
+  gsap.set(
+    [
+      "#F5_logo1-1",
+      "#F5_image1-1",
+      "#F5_text1-1",
+      "#F5_text1-2",
+      "#F5_text1-3",
+      ".cta",
+      "#F5-ctaHover1",
+    ],
+    { alpha: 1 }
+  );
+  gsap.set(["#F5_image2-1"], { alpha: 1, rotation: -8 });
+
+  gsap.to(["#F5_logo1-1", "#F5_image1-1"], {
+    duration: 1,
+    alpha: 1,
+  });
+  gsap.from(["#F5_image1-1"], {
+    duration: 5,
+    scale: 1.1,
+    force3D: false,
+    rotation: 0.01,
+  });
+
+  var tl = gsap.timeline({});
+  tl.from(["#F5_text1-1"], { duration: 0.5, x: -3, alpha: 0 });
+  tl.from(["#F5_text1-2"], { duration: 0.5, x: 3, alpha: 0 }, "-=0.1");
+  tl.from(["#F5_text1-3"], { duration: 0.5, x: -3, alpha: 0 }, "-=0.1");
+  tl.from(
+    [".cta", "#F5-ctaHover1"],
+    { duration: 0.5, x: -3, alpha: 0 },
+    "+=0.5"
+  );
+
+  addEventListeners();
+}
+
+function addEventListeners() {
+  creative.ad.addEventListener("mouseover", bannerOver);
+}
+
+function bannerOver(e) {
+  creative.ad.removeEventListener("mouseover", bannerOver);
+  /* Animate CTA */
+  gsap.to(["#sheen"], {
+    duration: 0.3,
+    x: 200,
+    repeat: 1,
+    yoyo: true,
+    onComplete: bannerOut,
+  });
+}
+
+function bannerOut(e) {
+  creative.ad.addEventListener("mouseover", bannerOver);
+  gsap.set(["#sheen"], { x: -120 });
+  /* CTA onComplete */
+}
