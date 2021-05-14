5a6958d21a664e06e75ae5733aa6e517f32d69ae
amay-zingg <anastasiamay29@gmail.com>
2021-05-14 01:46:58 -0400

    Updating  code

---
+++ _versionTwo/js/scripts.js
@@ -0,0 +1,7 @@
+/* * * * * * * * * * * * * * FRAME 1 * * * * * * * * * * * * * * */
+/* * * * Red BG - fade in lion face * * * */
+TweenMax.to('.logo-face', 3, { scale: 1.2, opacity: 1, ease: Back.easeIn});
+
+/* * * * * * * * * * * * * * FRAME 2 * * * * * * * * * * * * * * */
+/* * * * Red BG drops - reveals photo of group, group photo zooms in  * * * */
+TweenMax.to('.frame-2', 2, { scale: 1.2, opacity: 1, ease: Ease.easeIn});
